85d0eea9fcda3346f85a4c8cb8f6b30790fff7fa
hironomiu <hironomiu@gmail.com>
2022-03-12 02:22:11 +0900

    commited

--- src/js/libs/fizzBuzz.js
+++ src/js/libs/fizzBuzz.js
@@ -1,6 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const fizzBuzz = (num) => {
-    const result = num % 3 === 0 ? "Fizz" : num
-    return result
-}
-
-export default fizzBuzz
+    const result = num % 3 == 0 ? "Fizz" : num;
+    return result;
+};
+exports.default = fizzBuzz;
